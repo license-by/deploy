a61b5704caed9904fa870386ce4443752b5ce464
che <che.pasha@gmail.com>
2018-06-16 06:58:37 +0300

    AGPL and FDL has been added

--- letsencrypt/update.sh
+++ letsencrypt/update.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/home/license/letsencrypt/certbot/certbot-auto certonly --standalone --email info@hackerspace.by --renew-by-default --rsa-key-size 4096 -d license.by -d www.license.by -d apache.license.by -d bsd.license.by -d gpl.license.by -d lgpl.license.by -d mit.license.by -d mpl.license.by --pre-hook "systemctl stop nginx" --post-hook "systemctl start nginx"
+/home/license/letsencrypt/certbot/certbot-auto certonly --standalone --email info@hackerspace.by --renew-by-default --rsa-key-size 4096 -d license.by -d www.license.by -d agpl.license.by -d apache.license.by -d bsd.license.by -d fdl.license.by -d gpl.license.by -d lgpl.license.by -d mit.license.by -d mpl.license.by --pre-hook "systemctl stop nginx" --post-hook "systemctl start nginx"
